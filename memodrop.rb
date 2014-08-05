90aa7d3d01f0ec5792cb4004d3c2741bf159261a
Hikaru Watanabe <hikaru@paperboy.co.jp>
2014-08-05 23:36:58 +0900

    Initial Commit

---
+++ memodrop.rb
@@ -0,0 +1,38 @@
+# coding: utf-8
+require 'dropbox_sdk'
+require 'dotenv'
+
+class Memodrop
+  def initialize
+    @client = DropboxClient.new access_token
+  end
+
+  def main
+    @memo_dir = get_from_dropbox
+    selected = select_file_after Time.new(2014,8,4,0,0) # todo test
+    puts selected.map{|f|f["path"]}
+  end
+
+  private
+
+  def access_token
+    ENV["ACCESS_TOKEN"]
+  end
+
+  # 任意の日付以降に更新されたファイルを特定
+  def select_file_after(target_time)
+    @memo_dir["contents"].select do |file|
+      modified_time = DateTime.parse(file["modified"]).to_time
+      modified_time > target_time
+    end
+  end
+
+  def get_from_dropbox
+    @client.metadata "/memo/"
+  end
+
+  #contents, metadata = client.get_file_and_metadata('/memo/2014-03-30-study.md')
+end
+
+Dotenv.load
+Memodrop.new.main
