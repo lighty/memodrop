09402fbf37e390a89a3a0c21a50fe0fcad4c0baa
Hikaru Watanabe <hikaru@paperboy.co.jp>
2014-12-16 09:43:14 +0900

    configファイル作成

---
+++ config/dayone.rb
@@ -0,0 +1,15 @@
+class DayOne
+  def directory 
+    "Apps/Day One/Journal.dayone"
+  end
+
+  def convert(str_plist)
+    # plist形式のstrから本文を読み込んで返す。markdown->html変換は不要
+    str
+  end
+
+  def notebook
+    "dayone"
+  end
+
+end
---
+++ config/memolist.rb
@@ -0,0 +1,14 @@
+class Memolist
+  def directory 
+    "memo"
+  end
+
+  def convert(str)
+    str
+  end
+
+  def notebook
+    "memolist"
+  end
+
+end
