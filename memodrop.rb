93fd7ca41e64f3b0801fa73d8007451816a34eed
Hikaru Watanabe <hikaru@paperboy.co.jp>
2014-12-16 20:02:56 +0900

    fix require

--- memodrop.rb
+++ memodrop.rb
@@ -1,5 +1,5 @@
 # coding: utf-8
-Dir[File.join(File.dirname(__FILE__), "lib", "**/*.rb")].each{|f| require f }
+Dir[File.join(File.dirname(__FILE__), "lib", "**", "*.rb")].each{|f| require f }
 Dir[File.join(File.dirname(__FILE__), "config", "**", "*.rb")].each{|f| require f }
 require 'github/markdown'
 
