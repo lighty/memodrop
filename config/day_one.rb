9dee64a0ca677fec1d7ef93cea526548f498d73e
Hikaru Watanabe <hikaru@paperboy.co.jp>
2014-12-17 20:40:08 +0900

    ファイル内容をちゃんと返す

--- config/day_one.rb
+++ config/day_one.rb
@@ -1,11 +1,13 @@
+require 'plist'
 class DayOne
   def self.directory 
-    "Apps/Day One/Journal.dayone"
+    "Apps/Day One/Journal.dayone/entries"
   end
 
   def self.convert(str_plist)
     # plist形式のstrから本文を読み込んで返す。markdown->html変換は不要
-    str
+    result = Plist::parse_xml(str_plist)
+    result["Entry Text"]
   end
 
   def self.notebook
