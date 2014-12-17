a521f915a4ce672709946619be31bc47df4e797e
Hikaru Watanabe <hikaru@paperboy.co.jp>
2014-12-17 10:08:16 +0900

    ファイル名変更

--- config/day_one.rb
+++ config/day_one.rb
@@ -1,14 +1,14 @@
 class DayOne
-  def directory 
+  def self.directory 
     "Apps/Day One/Journal.dayone"
   end
 
-  def convert(str_plist)
+  def self.convert(str_plist)
     # plist形式のstrから本文を読み込んで返す。markdown->html変換は不要
     str
   end
 
-  def notebook
+  def self.notebook
     "dayone"
   end
 
