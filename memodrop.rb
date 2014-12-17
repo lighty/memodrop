e7206ee68c93299fd87a1c6cdf568f10994e12c9
Hikaru Watanabe <hikaru@paperboy.co.jp>
2014-12-17 10:08:36 +0900

    configファイルから読み込む

--- memodrop.rb
+++ memodrop.rb
@@ -2,6 +2,7 @@
 Dir[File.join(File.dirname(__FILE__), "lib", "**", "*.rb")].each{|f| require f }
 Dir[File.join(File.dirname(__FILE__), "config", "**", "*.rb")].each{|f| require f }
 require 'github/markdown'
+require 'active_support/core_ext'
 
 class String
   def nl2br
@@ -9,18 +10,29 @@ class String
   end
 end
 
-Dotenv.load
-files = Memodrop::Dropbox.new(Memolist).get_resent
-puts "connected dropbox"
-files.each_contents do |contents, filename|
-  puts filename
-  str = contents.force_encoding("UTF-8")
-  evernote = Memodrop::Evernote.new(Memolist)
-  content = evernote.markdownize str
-  p content
-  evernote.sync_note(filename, content, evernote.select_notebook)
+def configs
+  Dir[File.join(File.dirname(__FILE__), "config", "*.rb")].map{ |f| 
+    File.basename(f,".*").camelize
+  }
 end
-puts "connected evernote"
+
+Dotenv.load
+configs.each do |config|
+  puts "--#{config}--"
+  files = Memodrop::Dropbox.new(config.classify.constantize).get_resent
+  puts "connected dropbox"
+  files.each_contents do |contents, filename|
+    puts filename
+    str = contents.force_encoding("UTF-8")
+    evernote = Memodrop::Evernote.new(Memolist)
+    content = evernote.markdownize str
+    p content
+    evernote.sync_note(filename, content, evernote.select_notebook)
+  end
+  puts "connected evernote"
+end
 
 # TODO
 # デーモン化して1分おきに実行するようにする..
+
+
